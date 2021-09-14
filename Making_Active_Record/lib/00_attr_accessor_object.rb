aca7db4ef95bce330c09ff226f13519f2fb0ce34
gtsimis <gt2186@gmail.com>
2021-09-14 15:40:34 -0400

    Added getter fro my_attr_accessor

--- Making_Active_Record/lib/00_attr_accessor_object.rb
+++ Making_Active_Record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,12 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do 
+        name
+      end
+      define_method(name) do |new_val|
+        name = new_val
+      end
+    end
   end
 end
