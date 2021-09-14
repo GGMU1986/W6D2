bd800e5b69124743b9ad8b1bd4d5d88c648759a2
gtsimis <gt2186@gmail.com>
2021-09-14 17:48:24 -0400

    Added tables getter & setter

--- Making_Active_Record/lib/00_attr_accessor_object.rb
+++ Making_Active_Record/lib/00_attr_accessor_object.rb
@@ -2,10 +2,10 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |name|
       define_method(name) do 
-        name
+        instance_variable_get("@#{name}")
       end
-      define_method(name) do |new_val|
-        name = new_val
+      define_method("#{name}=") do |new_val| 
+        instance_variable_set("@#{name}", new_val)
       end
     end
   end
